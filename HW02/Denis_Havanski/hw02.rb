a201019c92795da494aaa2ff893f96ee5826e4b1
den-kak-den <dzianis.havanski@gmail.com>
2021-04-25 22:09:42 +0300

    Add my_select method

--- HW02/Denis_Havanski/hw02.rb
+++ HW02/Denis_Havanski/hw02.rb
@@ -1,3 +1,6 @@
+require 'pry'
+require 'pry-byebug'
+
 module HDArrayMethods
   refine Array do
 
@@ -6,16 +9,29 @@ module HDArrayMethods
         main_array = self
         ret_array = []
         last_el = self.size
+
         for i in 0...last_el
           ret_array << yield(main_array[i])
         end
+
         ret_array
-        end
       end
+    end
 
 
     def my_select
+      if block_given?
+        main_array = self
+        ret_array = []
+        last_el = self.size
 
+        for i in 0...last_el
+          if yield(main_array[i]) == true
+            ret_array << main_array[i]
+          end
+        end
+        ret_array
+      end
     end
 
     def my_each
@@ -27,7 +43,9 @@ end
 
 using HDArrayMethods
 
-array = [1,2,3]
-puts array.my_map{|e| e - e }
+array = [1,2,3,4,5,6,65]
 
+p array.my_map {|e| e**2 }
+
+p array.my_select {|e| e.even?}
 
