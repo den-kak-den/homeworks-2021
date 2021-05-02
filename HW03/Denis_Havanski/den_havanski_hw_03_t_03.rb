c29d761610ded48b47463157bd47740b4f6509e8
den-kak-den <dzianis.havanski@gmail.com>
2021-05-03 01:55:59 +0300

    Add a PART of task_3 method (v_1)

--- HW03/Denis_Havanski/den_havanski_hw_03_t_03.rb
+++ HW03/Denis_Havanski/den_havanski_hw_03_t_03.rb
@@ -1,1 +1,15 @@
+TIME_REG = /^[2]0[0-9]{2}\-[0-1]\d\-[0-3]\d\s[0-2]\d\:[0-5]\d\:[0-5]\d\.[0-9]/.freeze
 
+
+def task_3(logs)
+  array = logs.split("\n")
+  array.each do |timestamp|
+    #puts "timestamp #{timestamp}"
+    if timestamp.include? "Calling core"
+      puts "YES!"
+      timestamp = logs.slice(TIME_REG)
+      puts timestamp
+    end
+    end
+    return ''
+end
