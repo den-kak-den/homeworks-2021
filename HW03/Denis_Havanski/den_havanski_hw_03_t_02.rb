27ae4a46d48ab70032a2114055fefc3a057c0ae7
den-kak-den <dzianis.havanski@gmail.com>
2021-05-02 21:09:13 +0300

    Add task_2 method (v_1)

--- HW03/Denis_Havanski/den_havanski_hw_03_t_02.rb
+++ HW03/Denis_Havanski/den_havanski_hw_03_t_02.rb
@@ -1,1 +1,23 @@
+DATE_REG = /[0-3]{1}\d{1}\/[A-za-z]{3}\/[1-2]{1}\d{3}:[0-2]{1}[0-4]{1}:[0-5]{1}\d:[0-5]{1}\d\s\+[0-9]{4}/
+BODY_REG = /\/[a-z]+\/\d\/[a-z]+/
+IP_REG = /^[0-9]+\.[0-9]+\.[0-9]+\.[0-9]+/
 
+def task_2(logs)
+
+  array = logs.split("\n")
+
+  ret_array = []
+  array.each do |line|
+    date = line.slice(DATE_REG)
+    ip =  line.slice(IP_REG)
+    body = line.slice(BODY_REG)
+
+    if (date && ip && body).class == String
+      ret_line = "#{date} FROM: #{ip} TO: #{body.upcase}"
+      ret_array << ret_line
+    end
+  end
+
+  return ret_array
+
+end
