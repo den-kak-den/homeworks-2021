aae6e2a51a14ad89de9fcb8b7d677b123e29279b
den-kak-den <dzianis.havanski@gmail.com>
2021-05-03 03:37:51 +0300

    Add a MAIN of task_3 method (v_1): returns timeexp result.

--- HW03/Denis_Havanski/den_havanski_hw_03_t_03.rb
+++ HW03/Denis_Havanski/den_havanski_hw_03_t_03.rb
@@ -1,15 +1,29 @@
-TIME_REG = /^[2]0[0-9]{2}\-[0-1]\d\-[0-3]\d\s[0-2]\d\:[0-5]\d\:[0-5]\d\.[0-9]/.freeze
+#require 'date'
+require 'time'
+require 'pry-byebug'
 
+TIME_REG = /^[2]0[0-9]{2}\-[0-1]\d\-[0-3]\d\s[0-2]\d\:[0-5]\d\:[0-5]\d\.[0-9]/.freeze
 
 def task_3(logs)
   array = logs.split("\n")
-  array.each do |timestamp|
+  time_array = []
+  array.each do |line|
     #puts "timestamp #{timestamp}"
-    if timestamp.include? "Calling core"
+    if line.include? "Calling core"
       puts "YES!"
-      timestamp = logs.slice(TIME_REG)
-      puts timestamp
+      timestring = line.slice(TIME_REG)
+      timestamp = Time.parse(timestring)
+
+      time_array << timestamp
+      #puts timestamp.to_time
+
+      end
     end
-    end
+
+    puts time_array
+  #binding.pry
+       res = time_array[1] - time_array[0]
+    puts res
+
     return ''
 end
