9be2f413e3b7d6f7a34601b7ff7680de5ca064e0
den-kak-den <dzianis.havanski@gmail.com>
2021-05-03 04:56:21 +0300

    Add a FULL of task_3 method (v_1): all tests OK.

--- HW03/Denis_Havanski/den_havanski_hw_03_t_03.rb
+++ HW03/Denis_Havanski/den_havanski_hw_03_t_03.rb
@@ -1,29 +1,36 @@
-#require 'date'
+
 require 'time'
-require 'pry-byebug'
 
 TIME_REG = /^[2]0[0-9]{2}\-[0-1]\d\-[0-3]\d\s[0-2]\d\:[0-5]\d\:[0-5]\d\.[0-9]/.freeze
 
 def task_3(logs)
   array = logs.split("\n")
   time_array = []
+
   array.each do |line|
-    #puts "timestamp #{timestamp}"
     if line.include? "Calling core"
-      puts "YES!"
       timestring = line.slice(TIME_REG)
       timestamp = Time.parse(timestring)
-
       time_array << timestamp
-      #puts timestamp.to_time
-
       end
-    end
+  end
 
-    puts time_array
-  #binding.pry
-       res = time_array[1] - time_array[0]
-    puts res
+  ta_size = time_array.size
+  result = []
+  i = 0
+  while i < (ta_size - 1)
+    diff = -(time_array[i] - time_array[i + 1])
+    result << diff.to_s
+    i += 1
+    #puts i
+  end
 
-    return ''
+  if result.size == 1
+    return result[0]
+  elsif result.empty?
+    return "0"
+  else
+    return result
+  end
+
 end
