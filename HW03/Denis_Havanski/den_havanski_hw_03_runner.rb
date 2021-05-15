1236111725a6c61db404143ff352768caaecbb71
den-kak-den <dzianis.havanski@gmail.com>
2021-05-15 06:18:59 +0300

    Split method task3 into 3 methods

--- HW03/Denis_Havanski/den_havanski_hw_03_runner.rb
+++ HW03/Denis_Havanski/den_havanski_hw_03_runner.rb
@@ -39,8 +39,30 @@ logs = <<~INPUT
   2018-04-23 17:18:59.8 ubuntu-xenial[14319] Debug - inside docker_handle_event
 INPUT
 
+logs1 = <<~INPUT # Only one valid event
+  2018-04-23 17:17:49.7 ubuntu-xenial[14319] Debug - Calling core with action: event
+  2018-04-23 17:17:49.7 ubuntu-xenial[14319] Debug - connecting to: 10.6.246.101
+  2018-04-23 17:17:49.8 ubuntu-xenial[14319] Debug - Calling core docker event processed
+  2018-04-23 17:18:19.5 ubuntu-xenial[14319] Debug - monitoring grid communication health
+  2018-04-23 17:18:38.8 ubuntu-xenial[14319] Debug - Calling core with action: messages
+  2018-04-23 17:18:38.8 ubuntu-xenial[14319] Debug - connecting to: 10.6.246.101
+  2018-04-23 17:18:59.8 ubuntu-xenial[14319] Debug - Calling core inside docker_handle_event
+INPUT
+
+logs2 = <<~INPUT # Many valid events
+  2018-04-23 17:17:49.7 ubuntu-xenial[14319] Debug - Calling core with action: event
+  2018-04-23 17:17:49.7 ubuntu-xenial[14319] Debug - connecting to: 10.6.246.101
+  2018-04-23 17:17:49.8 ubuntu-xenial[14319] Debug - docker event processed
+  2018-04-23 17:18:19.5 ubuntu-xenial[14319] Debug - monitoring grid communication health
+  2018-04-23 17:18:38.8 ubuntu-xenial[14319] Debug -  with action: messages
+  2018-04-23 17:18:38.8 ubuntu-xenial[14319] Debug - connecting to: 10.6.246.101
+  2018-04-23 17:18:59.8 ubuntu-xenial[14319] Debug - inside docker_handle_event
+INPUT
+
 puts "\n----- Task_3 -----"
-puts task3(logs)
+puts "#{task3(logs)} \n"
+puts "Only one valid event: #{task3(logs1)} \n"
+puts "Many valid events: #{task3(logs2)} \n"
 
 #######################
 
--- HW03/Denis_Havanski/den_havanski_hw_03_t_03.rb
+++ HW03/Denis_Havanski/den_havanski_hw_03_t_03.rb
@@ -4,33 +4,30 @@ require 'time'
 
 TIME_REG = /^20[0-9]{2}-[0-1]\d-[0-3]\d\s[0-2]\d:[0-5]\d:[0-5]\d\.[0-9]/.freeze
 
-def task3(logs)
-  array = logs.split("\n")
+def create_time_array(array)
   time_array = []
-
   array.each do |line|
     next unless line.include? 'Calling core'
-
     timestring = line.slice(TIME_REG)
     timestamp = Time.parse(timestring)
     time_array << timestamp
   end
+  time_array
+end
 
-  # ta_size = time_array.size
-  # result = []
-  # i = 0
-  # while i < (ta_size - 1)
-  #   diff = -(time_array[i] - time_array[i + 1])
-  #   result << diff.to_s
-  #   i += 1
-  # end
-
+def create_arr_of_time_diff(time_array)
   result = []
   time_array.each_cons(2) do |el|
     diff = el[1] - el[0]
     result << diff.to_s
   end
+  result
+end
 
+def task3(logs)
+  array = logs.split("\n")
+  time_array = create_time_array(array)
+  result = create_arr_of_time_diff(time_array)
   if result.size == 1
     result[0]
   elsif result.empty?
