00a3ad6fa533c4b0b6630b44f8fe9b4dc8f3deff
den-kak-den <dzianis.havanski@gmail.com>
2021-05-24 02:57:28 +0300

    For HW02: add spec layout

--- HW02/Denis_Havanski/spec/array_methods_spec.rb
+++ HW02/Denis_Havanski/spec/array_methods_spec.rb
@@ -5,10 +5,47 @@ require_relative 'spec_helper'
 
 using ArrayMethods
 
-# так в RSpec начинается сценарий для конкретного класса/модуля/метода
 RSpec.describe ArrayMethods do
+  let(:number_arr) { [1, -4, 78421397493947923408, 34.4, 0] }
+  let(:mixed_arr) { ['sadfs', 645, :symb, {1=>'1'}, true] }
+  let(:empty_arr) { [] }
 
-  it 'should do ok for ...' do
-    puts "OK"
+  let(:block_sum_5) { :array.each {|el| el += 5} }
+  let(:block_multiply_10) { :array.each {|el| el *= 10}
+
+
+  describe '#my_map' do
+    context 'if no block given' do
+      expect()
+
+    end
+
+    context 'if block given' do
+      expect()
+    end
   end
+
+  describe '#my_select' do
+    context 'if no block given' do
+      expect()
+
+    end
+
+    context 'if block given' do
+      expect()
+    end
+  end
+
+  describe '#my_each' do
+    context 'if no block given' do
+      expect()
+
+    end
+
+    context 'if block given' do
+      expect()
+    end
+  end
+
+
 end
