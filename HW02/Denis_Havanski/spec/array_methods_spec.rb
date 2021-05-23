cf044dff84702249520ce7d9c1885b5e3445da19
den-kak-den <dzianis.havanski@gmail.com>
2021-05-24 01:40:32 +0300

    For HW02: plug RSpec, add spec files

---
+++ HW02/Denis_Havanski/spec/array_methods_spec.rb
@@ -0,0 +1,14 @@
+require 'rspec'
+
+require_relative '../array_methods'
+require_relative 'spec_helper'
+
+using ArrayMethods
+
+# так в RSpec начинается сценарий для конкретного класса/модуля/метода
+RSpec.describe ArrayMethods do
+
+  it 'should do ok for ...' do
+    puts "OK"
+  end
+end
