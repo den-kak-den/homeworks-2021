d63b6cf66ecfc3138740c616a2f2a637f43c2fd9
den-kak-den <dzianis.havanski@gmail.com>
2021-06-06 22:59:56 +0300

    den_havanski_hw_03_t_01_spec.rb: rewrite lets to contexts, add contexts and describe block

--- HW03/Denis_Havanski/spec/den_havanski_hw_03_t_01_spec.rb
+++ HW03/Denis_Havanski/spec/den_havanski_hw_03_t_01_spec.rb
@@ -6,57 +6,71 @@ require_relative 'spec_helper'
 RSpec.describe 'task1' do
   describe '#return_error' do
 
-  let(:log_with_upcase_error) do <<~INPUT
-      10.6.246.103 - - [23/Apr/2018:20:30:39 +0300] "POST /grid/2/messages HTTP/1.1" 200 48 0.0498
-      10.6.246.101 - - [23/Apr/2018:20:30:42 +0300] "POST /grid/2/event HTTP/1.1" 200 - 0.2277
-      2018-04-23 20:30:42: SSL error, peer: 10.6.246.101, peer cert: , #<Puma::MiniSSL::SSLError: System ERROR: Undefined error: 0 - 0>
-      10.6.246.101 - - [23/Apr/2018:20:29:39 +0300] "POST /grid/2/messages HTTP/1.1" 200 48 0.0290
-  INPUT
-  end
-
-  let(:log_with_downcase_error) do <<~INPUT
-    10.6.246.103 - - [23/Apr/2018:20:30:39 +0300] "POST /grid/2/messages HTTP/1.1" 200 48 0.0498
-    10.6.246.101 - - [23/Apr/2018:20:30:42 +0300] "POST /grid/2/event HTTP/1.1" 200 - 0.2277
-    2018-04-23 20:30:42: SSL error, peer: 10.6.246.101, peer cert: , #<Puma::MiniSSL::SSLError: System error: Undefined error: 0 - 0>
-    10.6.246.101 - - [23/Apr/2018:20:29:39 +0300] "POST /grid/2/messages HTTP/1.1" 200 48 0.0290
-  INPUT
-  end
-
-  let(:log_without_error) do <<~INPUT
-    10.6.246.103 - - [23/Apr/2018:20:30:39 +0300] "POST /test/2/messages HTTP/1.1" 200 48 0.0498
-    10.6.246.101 - - [23/Apr/2018:20:30:42 +0300] "POST /test/2/run HTTP/1.1" 200 - 0.2277
-    10.6.246.101 - - [23/Apr/2018:20:31:39 +0300] "POST /test/2/messages HTTP/1.1" 200 48 0.0290
-  INPUT
-  end
-
-  let(:log_with_some_errors) do <<~INPUT
-    10.6.246.103 - - [23/Apr/2018:20:30:39 +0300] "POST /grid/2/messages HTTP/1.1" 200 48 0.0498
-    10.6.246.101 - - [23/Apr/2018:20:30:42 +0300] "POST /grid/2/event HTTP/1.1" 200 - 0.2277
-    2018-04-23 20:30:42: SSL error, peer: 10.6.246.101, peer cert: , #<Puma::MiniSSL::SSLError: System error: Undefined error: 0 - 0>
-    10.6.246.101 - - [23/Apr/2018:20:29:39 +0300] "POST /grid/2/messages HTTP/1.1" 200 48 0.0290
-    2018-04-23 20:50:42: SSL error, peer: 10.6.246.101, peer cert: , #<Puma::MiniSSL::SSLError: System error: Undefined error: 0 - 0>
-  INPUT
-  end
-
-    context 'when log contains error' do
-      it 'return full text of the 1st line with error_in_downcase' do
-        expect( task1(log_with_downcase_error) ).to eql('2018-04-23 20:30:42: SSL error, peer: 10.6.246.101, peer cert: , #<Puma::MiniSSL::SSLError: System error: Undefined error: 0 - 0>')
+    context 'when log contains downcase errors' do
+      let(:log_with_downcase_error) do <<~INPUT
+        10.6.246.103 - - [23/Apr/2018:20:30:39 +0300] "POST /grid/2/messages HTTP/1.1" 200 48 0.0498
+        10.6.246.101 - - [23/Apr/2018:20:30:42 +0300] "POST /grid/2/event HTTP/1.1" 200 - 0.2277
+        2018-04-23 20:30:42: SSL error, peer: 10.6.246.101, peer cert: , #<Puma::MiniSSL::SSLError: System error: Undefined error: 0 - 0>
+        10.6.246.101 - - [23/Apr/2018:20:29:39 +0300] "POST /grid/2/messages HTTP/1.1" 200 48 0.0290
+      INPUT
       end
 
-      it 'return full text of the 1st line with error in UPcase' do
-        expect( task1(log_with_upcase_error) ).to eql('2018-04-23 20:30:42: SSL error, peer: 10.6.246.101, peer cert: , #<Puma::MiniSSL::SSLError: System ERROR: Undefined error: 0 - 0>')
+      let(:expected_output) {'2018-04-23 20:30:42: SSL error, peer: 10.6.246.101, peer cert: , #<Puma::MiniSSL::SSLError: System error: Undefined error: 0 - 0>'}
+
+        it 'return first line with error in downcase' do
+          expect( task1(log_with_downcase_error) ).to eq(expected_output)
+        end
+    end
+
+    context 'when log contains upcase errors' do
+      let(:log_with_upcase_error) do <<~INPUT
+        10.6.246.103 - - [23/Apr/2018:20:30:39 +0300] "POST /grid/2/messages HTTP/1.1" 200 48 0.0498
+        10.6.246.101 - - [23/Apr/2018:20:30:42 +0300] "POST /grid/2/event HTTP/1.1" 200 - 0.2277
+        2018-04-23 20:30:42: SSL error, peer: 10.6.246.101, peer cert: , #<Puma::MiniSSL::SSLError: System ERROR: Undefined error: 0 - 0>
+        10.6.246.101 - - [23/Apr/2018:20:29:39 +0300] "POST /grid/2/messages HTTP/1.1" 200 48 0.0290
+        INPUT
       end
 
-      it 'return full text of the 1st line with error from some lines with errors' do
-        expect( task1(log_with_some_errors)).to eql('2018-04-23 20:30:42: SSL error, peer: 10.6.246.101, peer cert: , #<Puma::MiniSSL::SSLError: System error: Undefined error: 0 - 0>')
+      let(:expected_output) {'2018-04-23 20:30:42: SSL error, peer: 10.6.246.101, peer cert: , #<Puma::MiniSSL::SSLError: System ERROR: Undefined error: 0 - 0>'}
+
+      it 'return first line with error in upcase' do
+        expect( task1(log_with_upcase_error) ).to eq(expected_output)
       end
     end
 
+    context 'when log contains different errors' do
+      let(:log_with_some_errors) do <<~INPUT
+        10.6.246.103 - - [23/Apr/2018:20:30:39 +0300] "POST /grid/2/messages HTTP/1.1" 200 48 0.0498
+        10.6.246.101 - - [23/Apr/2018:20:30:42 +0300] "POST /grid/2/event HTTP/1.1" 200 - 0.2277
+        2018-04-23 20:30:42: SSL error, peer: 10.6.246.101, peer cert: , #<Puma::MiniSSL::SSLError: System error: Undefined error: 0 - 0>
+        10.6.246.101 - - [23/Apr/2018:20:29:39 +0300] "POST /grid/2/messages HTTP/1.1" 200 48 0.0290
+        2018-04-23 20:50:42: SSL error, peer: 10.6.246.101, peer cert: , #<Puma::MiniSSL::SSLError: System error: Undefined error: 0 - 0>
+      INPUT
+      end
+
+      let(:expected_output) {'2018-04-23 20:30:42: SSL error, peer: 10.6.246.101, peer cert: , #<Puma::MiniSSL::SSLError: System error: Undefined error: 0 - 0>'}
+
+        it 'return only first line with errors' do
+          expect( task1(log_with_some_errors)).to eq(expected_output)
+        end
+    end
+  end
+
+  describe '#return_if_no_errors' do
+
     context 'when log does not contains errors' do
+      let(:log_without_errors) do <<~INPUT
+        10.6.246.103 - - [23/Apr/2018:20:30:39 +0300] "POST /test/2/messages HTTP/1.1" 200 48 0.0498
+        10.6.246.101 - - [23/Apr/2018:20:30:42 +0300] "POST /test/2/run HTTP/1.1" 200 - 0.2277
+        10.6.246.101 - - [23/Apr/2018:20:31:39 +0300] "POST /test/2/messages HTTP/1.1" 200 48 0.0290
+      INPUT
+      end
+
       it 'return empty string' do
-        expect(task1(log_without_error)).to eql('')
+        expect(task1(log_without_errors)).to eql('')
       end
     end
   end
 end
+
 
